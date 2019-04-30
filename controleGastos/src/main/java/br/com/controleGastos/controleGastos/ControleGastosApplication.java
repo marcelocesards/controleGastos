8db2d090fef7222d132ffcef3dd51cc344ddbc15
Marcelo Cesar <marcelo.cesards@gmail.com>
2019-04-30 20:37:16 -0300

    Criação do projeto pelo spring initializer

---
+++ controleGastos/src/main/java/br/com/controleGastos/controleGastos/ControleGastosApplication.java
@@ -0,0 +1,13 @@
+package br.com.controleGastos.controleGastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleGastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleGastosApplication.class, args);
+	}
+
+}

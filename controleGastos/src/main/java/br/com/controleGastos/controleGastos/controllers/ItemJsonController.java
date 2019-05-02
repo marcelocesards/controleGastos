405aa1c4d1a73092cc6f3de4e00e1879b89fcd61
Marcelo Cesar <marcelo.cesards@gmail.com>
2019-05-02 08:23:27 -0300

    Implementando JMS

--- controleGastos/src/main/java/br/com/controleGastos/controleGastos/controllers/ItemJsonController.java
+++ controleGastos/src/main/java/br/com/controleGastos/controleGastos/controllers/ItemJsonController.java
@@ -1,14 +1,19 @@
 package br.com.controleGastos.controleGastos.controllers;
 
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.controleGastos.controleGastos.jms.consumer.Producer;
 import br.com.controleGastos.controleGastos.model.Item;
 import br.com.controleGastos.controleGastos.service.ItemService;
 import io.swagger.annotations.ApiOperation;
@@ -20,6 +25,8 @@ import io.swagger.annotations.ApiResponses;
 public class ItemJsonController {
 	@Autowired
 	private ItemService service;
+	
+	@Autowired private JmsTemplate jmsTemplate;
 	
 	@ApiOperation(value = "Faz um novo lançamento", 
 			notes = "Faz um novo lançamento de depesa ou provento",
@@ -33,6 +40,12 @@ public class ItemJsonController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Item create(@RequestBody Item item){
 		service.save(item);
+		try {
+			new Producer().testeJMS(item);
+		} catch (NamingException | JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return item;
 	}
 }
